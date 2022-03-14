8e038b8427e4a1772e719014b12daec1d075732a
BECKQuentin <beckquentin26@gmail.com>
2022-03-14 14:56:26 +0100

    exo 5 done

--- main.js
+++ main.js
@@ -32,13 +32,32 @@ function tick() {
 setInterval(tick, 1000);
 
 
+function Clock(props) {
+    React.useEffect(() => {
+        tick();
+    }, []);
 
+    const [date, setDate] = React.useState(new Date());
 
+    const tick = () => {
+        setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+    }
 
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <h2>Il est {date.toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+
+ReactDOM.render(<Clock />, document.querySelector('#app'));
 
 
 /*Component*/
-class Tick extends React.Component {
+class Clock extends React.Component {
     props;
     constructor(props) {
         super(props);
