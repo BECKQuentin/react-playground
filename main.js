0d1b1fd2c6926e1c5df09845b2f58489563897f4
BECKQuentin <beckquentin26@gmail.com>
2022-03-14 14:48:07 +0100

    exo 5 done

--- main.js
+++ main.js
@@ -3,7 +3,54 @@ function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello World !</h1>
+            <h2>Il est {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+// Chaque seconde, nous appellons ReactDOM.render() depuis un callback passé à setInterval().
+setInterval(tick, 1000);
+function Clock(props) {
+    return (
+        <div>
+            <h1>Bonjour, monde !</h1>
+            <h2>Il est {props.date.toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+
+function tick() {
+    ReactDOM.render(
+        <Clock date={new Date()} />,
+        document.getElementById('root')
+    );
+}
+setInterval(tick, 1000);
+
+
+
+
+
+
+
 /*Component*/
+class Tick extends React.Component {
+    props;
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div>
+            <h1>Hello World !</h1>
+            <h2>Il est {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    }
+}
+
 class FirstName extends React.Component {
     props;
     constructor(props) {
