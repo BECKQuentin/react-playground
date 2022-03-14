79771f46b2ae68ebe88b432d1f9624cf530d223f
BECKQuentin <beckquentin26@gmail.com>
2022-03-14 13:24:13 +0100

    exo 1 done

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+import * as ReactDOM from "react-dom";
 
+const name = 'Quentin BECK'
+const contact =  <h1>Bonjour, je m\'appelles {name}</h1>
+
+const contactTitleElement = React.createElement(
+    contact,{}, name
+)
 
 const title = React.createElement(
     'h1',
@@ -8,6 +15,11 @@ const title = React.createElement(
     'Hello world'
 )
 let content = document.getElementById('content')
+let contactTitle = document.getElementById('contact_title')
+
 ReactDOM.render(title, content)
+ReactDOM.render(contactTitleElement, contactTitle)
 
 console.log(title)
+
+// Créer une fonction qui va nous permettre de retourner nos Strings dans le format souhaité
