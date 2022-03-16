54754d10e1228c3227c3f53d5d8dc37a7b1006ad
BECKQuentin <beckquentin26@gmail.com>
2022-03-16 12:03:13 +0100

    exo 9 done

--- main.js
+++ main.js
@@ -67,19 +67,115 @@
 //     }
 // }
 
-function NameForm(props) {
+// function NameForm(props) {
+//
+//     const [name, setName] = React.useState('')
+//
+//     function handleChange(e)
+//     {
+//         e.preventDefault();
+//         setName(e.target.value);
+//     }
+//     function handleSubmit(e)
+//     {
+//         alert('Le nom a été soumis : ' + name);
+//         event.preventDefault();
+//     }
+//
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <label>
+//                 Nom :
+//                 <input type="text" value={name} onChange={handleChange}/>
+//             </label>
+//             <input type="submit" value="Envoyer" />
+//         </form>
+//     );
+// }
+//
+// function EssayForm(props) {
+//
+//     const [essay, setEssay] = React.useState('Écrivez un essai à propos de votre élément du DOM préféré')
+//
+//     function handleChange(e)
+//     {
+//         e.preventDefault();
+//         setEssay(e.target.value);
+//     }
+//     function handleSubmit(e)
+//     {
+//         alert('Un essai a été envoyé : ' + name);
+//         e.preventDefault();
+//     }
+//
+//     return(
+//         <form onSubmit={handleSubmit}>
+//             <label>
+//                 Essay:
+//                 <textarea rows={5} cols={35} value={essay} onChange={handleChange} />
+//             </label>
+//             <input type="submit" value="Envoyer" />
+//         </form>
+//     )
+// }
+//
+// function FlavorForm(props) {
+//
+//     // let opt1 = document.getElementById("opt1")
+//
+//     const [flavor, setFlavor] = React.useState()
+//
+//     function handleChange(e)
+//     {
+//         setFlavor(e.target.value)
+//     }
+//
+//     function handleSubmit(e)
+//     {
+//         e.preventDefault();
+//         if(flavor == 'default') return
+//
+//         alert('Votre parfum favori est : ' + flavor);
+//     }
+//
+//     return (
+//         <form onSubmit={handleSubmit}>
+//             <label>
+//                 Choisissez votre parfum favori :
+//                 <select value={flavor} onChange={handleChange} required={true}>
+//                     <option value="" disabled>Choississez une Saveur</option>
+//                     <option value="grapefruit">Pamplemousse</option>
+//                     <option value="lime">Citron vert</option>
+//                     <option value="coconut">Noix de coco</option>
+//                     <option value="mango">Mangue</option>
+//                 </select>
+//             </label>
+//             <input type="submit" value="Envoyer" />
+//         </form>
+//     );
+// }
 
+function Form(props)
+{
     const [name, setName] = React.useState('')
+    const [essay, setEssay] = React.useState('Écrivez un essai à propos de votre élément du DOM préféré')
+    const [flavor, setFlavor] = React.useState('coconut')
 
     function handleChange(e)
     {
-        e.preventDefault();
-        setName(e.target.value);
+        const target = e.target;
+        console.log(e)
+        // setName(target.value);
+        // setEssay(target.value);
+        // setFlavor(target.value)
     }
+
     function handleSubmit(e)
     {
-        alert('Le nom a été soumis : ' + name);
-        event.preventDefault();
+        e.preventDefault();
+        if(flavor == 'default') return
+
+        alert('Votre parfum favori est : ' + flavor);
     }
 
     return (
@@ -88,36 +184,28 @@ function NameForm(props) {
                 Nom :
                 <input type="text" value={name} onChange={handleChange}/>
             </label>
-            <input type="submit" value="Envoyer" />
-        </form>
-    );
-}
-
-function EssayForm(props) {
-
-    const [essay, setEssay] = React.useState('Écrivez un essai à propos de votre élément du DOM préféré')
-
-    function handleChange(e)
-    {
-        e.preventDefault();
-        setEssay(e.target.value);
-    }
-    function handleSubmit(e)
-    {
-        alert('Un essai a été envoyé : ' + name);
-        e.preventDefault();
-    }
-
-    return (
-        <form onSubmit={handleSubmit}>
+            <hr/>
             <label>
                 Essay:
-                <textrea rows={5} cols={35} value={essay} onChange={handleChange} />
+                <textarea rows={5} cols={35} value={essay} onChange={handleChange} />
+            </label>
+            <hr/>
+            <label>
+                Choisissez votre parfum favori :
+                <select value={flavor} onChange={handleChange} required={true}>
+                    <option value="" disabled>Choississez une Saveur</option>
+                    <option value="grapefruit">Pamplemousse</option>
+                    <option value="lime">Citron vert</option>
+                    <option value="coconut">Noix de coco</option>
+                    <option value="mango">Mangue</option>
+                </select>
             </label>
             <input type="submit" value="Envoyer" />
         </form>
     );
 }
 
-ReactDOM.render(<NameForm/>, document.querySelector('#app'))
-ReactDOM.render(<EssayForm/>, document.querySelector('#app'))
+// ReactDOM.render(<NameForm/>, document.querySelector('#app'))
+// ReactDOM.render(<EssayForm/>, document.querySelector('#app'))
+// ReactDOM.render(<FlavorForm />, document.querySelector('#app'))
+ReactDOM.render(<Form />, document.querySelector('#app'))
