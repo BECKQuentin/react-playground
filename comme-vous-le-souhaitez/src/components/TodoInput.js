8dd236b3e5f1e1ce56b0419a031617f9c8615333
BECKQuentin <beckquentin26@gmail.com>
2022-03-18 09:59:43 +0100

    exo 9 done

--- comme-vous-le-souhaitez/src/components/TodoInput.js
+++ comme-vous-le-souhaitez/src/components/TodoInput.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 export default function TodoInput() {
 
+  const [input, setInput] = React.useState('')
 
-
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    alert('soumis')
+  }
+  useEffect(() => {
+    console.log(input)
+  },[input])
 
   return (
   <div>
     <label htmlFor="input">Rentrez une tâche : </label>
     <input id="input" type="text" onChange={handleChange} />
+    <button type="submit" onSubmit={handleSubmit}>Ajouter</button>
   </div>);
 }
