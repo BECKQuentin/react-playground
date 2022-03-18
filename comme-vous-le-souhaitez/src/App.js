7596126ac2b2319667a2a82102ffd6a766562cb2
BECKQuentin <beckquentin26@gmail.com>
2022-03-18 15:39:44 +0100

    exo 9 done

--- comme-vous-le-souhaitez/src/App.js
+++ comme-vous-le-souhaitez/src/App.js
@@ -1,9 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
 import TodoInput from "./components/TodoInput";
+import React from "react";
 
 function App() {
 
+    const [input, setInput] = React.useState('')
+    const [newtodo, setNewtodo] = React.useState('')
+
+    const handleChange = (e) => {
+        setInput(e.target.value)
+    }
+    const handleClick = (e) => {
+
+    }
+    const handleKeyDown = (e) => {
+        if(e.key == 'Enter') {
+            setNewtodo(input)
+            setInput('')
+        } else if (e.key == 'Escape') {
+            setInput('')
+            console.log(input)
+        }
+    }
 
   return (
     <div className="App">
@@ -12,7 +31,10 @@ function App() {
         - le composant qui affiche les filtres
         - La liste des todos 
       */}
-      <TodoInput />
+      <TodoInput
+        onChangeBase={handleChange}
+        onKeyDownBase={handleKeyDown}
+        onClickBase={handleClick}/>
     </div>
   );
 }
--- comme-vous-le-souhaitez/src/components/TodoInput.js
+++ comme-vous-le-souhaitez/src/components/TodoInput.js
@@ -1,26 +1,13 @@
 import React, {useEffect} from "react";
 
-export default function TodoInput() {
+export default function TodoInput({onChangeBase, onKeyDownBase, onClickBase}) {
 
-  const [input, setInput] = React.useState('')
-  const [newtodo, setNewtodo] = React.useState('')
 
-  const handleChange = (e) => {
-    setInput(e.target.value)
-  }
-  const handleKeyDown = (e) => {
-    if(e.key == 'Enter') {
-      setNewtodo(input)
-      setInput('')
-    } else if (e.key == 'Escape') {
-      setInput('')
-      console.log(input)
-    }
-  }
 
   return (
   <div>
     <label htmlFor="input">Rentrez une tâche : </label>
-    <input id="input" type="text" onChange={handleChange} onKeyDown={handleKeyDown} />
+    <input id="input" type="text" onChange={onChangeBase} onKeyDown={onKeyDownBase} />
+    <input type="submit" onClick={onClickBase} />
   </div>);
 }
