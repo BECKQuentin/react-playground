61a069ba144211e476b04036b0304c23e497586b
BECKQuentin <beckquentin26@gmail.com>
2022-03-18 10:28:35 +0100

    exo 9 done

--- comme-vous-le-souhaitez/src/components/TodoInput.js
+++ comme-vous-le-souhaitez/src/components/TodoInput.js
@@ -3,21 +3,29 @@ import React, {useEffect} from "react";
 export default function TodoInput() {
 
   const [input, setInput] = React.useState('')
+  const [newTodo, setNewTodo] = React.useState('')
 
   const handleChange = (e) => {
     setInput(e.target.value)
   }
-  const handleSubmit = (e) => {
-    alert('soumis')
+  const handleKeyDown = (e) => {
+    console.log(e.key)
+    if(e.key == 'Enter') {
+      setNewTodo(input)
+      setInput('')
+      console.log(newTodo)
+    } else if (e.key == 'Escape') {
+      setInput('')
+      // console.log(input)
+    }
   }
-  useEffect(() => {
-    console.log(input)
-  },[input])
+  // useEffect(() => {
+  //   console.log(input)
+  // },[input])
 
   return (
   <div>
     <label htmlFor="input">Rentrez une tâche : </label>
-    <input id="input" type="text" onChange={handleChange} />
-    <button type="submit" onSubmit={handleSubmit}>Ajouter</button>
+    <input id="input" type="text" onChange={handleChange} onKeyDown={handleKeyDown} />
   </div>);
 }
